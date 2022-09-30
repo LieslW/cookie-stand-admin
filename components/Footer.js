3748ab8a94146fbd817b9f16ea3b22e1acbfee33
Liesl White <lieslfwhite@gmail.com>
2022-09-30 00:37:18 -0700

    footer added and styled

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer(props) {
+    return (
+      <header className="flex py-4 px-10 display bg-emerald-400">
+        <h1 className="w-1/2 text-3s font-bold text-gray-700 ">
+          @2021
+        </h1>
+        <div className="flex w-1/2 space-x-3 text-right">
+        </div>
+      </header>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 
 export default function Home() {
@@ -9,9 +10,8 @@ export default function Home() {
         <title>Cookie Stand Admin</title>
       </Head>
       <Header />
-      <header></header>
       <main></main>
-      <footer></footer>
+      <Footer />
     </div>
   )
 }
