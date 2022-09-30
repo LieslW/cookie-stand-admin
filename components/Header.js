85e6f25c6569eb38c2534304e224b47554bc7a00
Liesl White <lieslfwhite@gmail.com>
2022-09-30 00:32:20 -0700

    header styling and intialization done

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+export default function Header(props) {
+    return (
+      <header className="flex py-4 px-10 display bg-emerald-400">
+        <h1 className="w-1/2 text-3xl font-bold text-gray-700 ">
+          Cookie Stand Admin
+        </h1>
+        <div className="flex w-1/2 space-x-3 text-right">
+        </div>
+      </header>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from "../components/Header";
+
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Cookie Stand Admin</title>
+      </Head>
+      <Header />
       <header></header>
       <main></main>
       <footer></footer>
